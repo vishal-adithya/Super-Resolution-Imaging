ed65b1a5d94fdbb50aa945ccc30301b0de4ef8b4
Adi <vishaladithya2008@gmail.com>
2024-07-11 23:19:47 +0530

    final commit

--- utils.py
+++ utils.py
@@ -1,4 +1,12 @@
+import tensorflow as tf
 
 
 def NORMALIZATION_FUNC(x):
     return x / 255.0 
+
+
+class CALLBACK_TOOL():
+
+    def MODEL_CHECKPOINT(path):
+        x = tf.keras.callbacks.ModelCheckpoint(filepath=path)
+        return x
